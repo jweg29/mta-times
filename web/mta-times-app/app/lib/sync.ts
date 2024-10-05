b90bf80caef5fe2634cfb7068f53bfd2023ca6b8
James Wegner <jweg29@gmail.com>
2024-10-04 21:51:41 -0400

    fix packages

--- web/mta-times-app/app/lib/sync.ts
+++ web/mta-times-app/app/lib/sync.ts
@@ -1,8 +1,24 @@
+import path from 'path';
+import { Stop } from "./definitions";
+import { fetchAllStops } from "./gtfsHelpers/stops";
+import { parseCSV } from "./utils";
+
 /**
  * Syncs data from the static gtfs files and populates the database.
  */
 export const syncGTFSData = async () => {
+    // Stations data
+    const stationsPath = path.join(process.cwd(), 'app', 'lib', 'staticGTFS', 'stations.csv');
+    const parsedStops: Stop[] = parseCSV(stationsPath);
 
+    // Setup Stops
+    const stops = await fetchAllStops();
+
+    // Setup Routes
+
+    // Setup StopTimes
+
+    // Setup Trips
 };
 
 /**
