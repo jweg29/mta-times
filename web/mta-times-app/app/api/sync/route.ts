fead78c0556eef210be47b0ac7ce264d32e4ad3f
James Wegner <jweg29@gmail.com>
2024-10-04 21:37:00 -0400

    setting up prisma and database

---
+++ web/mta-times-app/app/api/sync/route.ts
@@ -0,0 +1,17 @@
+import { syncGTFSData } from 'lib/sync';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+    console.log(`syncing...⏳`)
+
+    try {
+        await syncGTFSData();
+        return NextResponse.json({ "response": "synced ✅" }, { status: 200 });
+    } catch (error) {
+        console.error(`Failed to sync GTFS data: ${error}`);
+        return NextResponse.json(
+            { error: `Failed to sync GTFS data: ${error}` },
+            { status: 500 }
+        );
+    }
+}
---
+++ web/mta-times-app/app/lib/sync.ts
@@ -0,0 +1,13 @@
+/**
+ * Syncs data from the static gtfs files and populates the database.
+ */
+export const syncGTFSData = async () => {
+
+};
+
+/**
+ * Downloads and updates the latest static GTFS files.
+ */
+export const updateStaticGTFS = async () => {
+
+}
