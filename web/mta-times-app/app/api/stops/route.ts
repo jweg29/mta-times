a98afa1c34e50a53d24a5cca994dfb1b406d3403
James Wegner <jweg29@gmail.com>
2024-10-14 19:33:50 -0400

    fix stops?

--- web/mta-times-app/app/api/stops/route.ts
+++ web/mta-times-app/app/api/stops/route.ts
@@ -1,4 +1,4 @@
-import { Route, Stop } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import { fetchStopByLatLon, fetchStops } from 'lib/gtfsHelpers/stops';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -7,13 +7,14 @@ export async function GET(request: NextRequest) {
         const lat = request.nextUrl.searchParams.get("lat") as string;
         const lon = request.nextUrl.searchParams.get("lon") as string;
 
-        let stops: Stop & { stop: Route[] }[]
+        let stops: (Prisma.StopGetPayload<{ include: { entrances: true, routes: true } }>)[]
         if (lat != undefined && lon != undefined) {
             console.log(`/api/stops fetchStopByLatLon`)
-            stops = await fetchStopByLatLon(Number(lat), Number(lon)) as unknown as Stop & { stop: Route[] }[];
+            stops = await fetchStopByLatLon(Number(lat), Number(lon)) as unknown as (Prisma.StopGetPayload<{ include: { entrances: true, routes: true } }>)[];
         } else {
             console.log(`/api/stops fetchStops`)
-            stops = await fetchStops() as unknown as Stop & { stop: Route[] }[];
+
+            stops = await fetchStops() as unknown as (Prisma.StopGetPayload<{ include: { entrances: true, routes: true } }>)[];
         }
 
         return NextResponse.json(
